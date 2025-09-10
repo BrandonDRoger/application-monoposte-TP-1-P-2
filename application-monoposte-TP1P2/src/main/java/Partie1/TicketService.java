4f5856e6a32a03540869a597c7262ebd68f168c4
BrandonDRoger <acnologiaft9@gmail.com>
2025-09-09 21:57:03 -0400

    Creation de la classe TicketService

---
+++ application-monoposte-TP1P2/src/main/java/Partie1/TicketService.java
@@ -0,0 +1,18 @@
+package Partie1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TicketService {
+    @Autowired TicketRepository ticketRepository;
+    /**
+     * Récupère tous les tickets en base de données
+     * @return liste de tous les tickets
+     */
+    public List<Ticket> obtenirTousLesTickets() {
+        return ticketRepository.findAll();
+    }
+}
